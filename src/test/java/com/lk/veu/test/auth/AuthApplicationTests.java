d1f62a048bd68d2177e21f46640a6948c3c7a62a
iran jayanindu <iran.nilame@arimaclanka.com>
2023-03-18 02:05:02 +0530

    step one

---
+++ src/test/java/com/lk/veu/test/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lk.veu.test.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
